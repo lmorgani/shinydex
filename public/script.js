4e0b95cfe11da7957a57534d2749492675d84d49
lmorgani <75508271+lmorgani@users.noreply.github.com>
2022-02-22 11:55:53 -0500

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+// This code was adapted from the Bob's Burgers Search code (https://github.com/nsitu/BobsBurgersSearch)
+
 const container = document.querySelector('#mons');
 
 axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=1000')
@@ -5,6 +7,7 @@ axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=1000')
 
     // an array of character objects will be stored in this variable. 
     let mons = response.data;
+
 console.log(mons);
 
     // let's create an array with just the names of the characters (strings only)
@@ -19,12 +22,18 @@ console.log(pokemonNames);
 
     // create the autocomplete using the character data. 
     const autoCompleteJS = new autoComplete({
-      placeHolder: "Search for Pokemon...",
+      placeHolder: "Search for Pokémon...",
           data: {
               src: pokemonNames
-          }
-      });
-      // whenever a character is selected, 
+          },
+          // This code is from https://tarekraafat.github.io/autoComplete.js/ to target the number of results given in the autocomplete dropdown.
+      resultsList: {
+          maxResults: 15,
+    }
+  });
+      
+
+            // whenever a character is selected, 
       // find the character in the characters array,
       // and pass the data along for rendering
       autoCompleteJS.input.addEventListener("selection",  (event) => {
@@ -32,40 +41,83 @@ console.log(pokemonNames);
         // whose name matches the autocomplete. 
         // see also: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
        
-        const [selected] = pokemonNames.filter( 
+        const [selected] = mons.results.filter( 
             pokemon => pokemon.name == event.detail.selection.value 
           ) ;
-          console.log(event.detail.selection.value);
+          console.log(selected);
+        // pokemonNames.includes(event.detail.selection.value)
+        // console.log(pokemonNames.includes(event.detail.selection.value))
+        // let test = pokemonNames.get(event.detail.selection.value)
+        // console.log(test)
         renderPokemon(selected); 
+
       }); 
+
   })
   .catch(function (error) {
     // handle error
     console.log(error);
   })
 
-
   const renderPokemon  = (pokemon) => {
     console.log(pokemon)
 
-    axios.get('pokemon.url')
+    axios.get(pokemon.url)
     .then(function (response) {
-console.log(pokemon.url)
+console.log(response.data)
+let pokemon = response.data
+let endpoint = pokemon.location_area_encounters
+
+axios.get(endpoint)
+.then(function (response) {
+let encounters = response.data
+console.log(encounters)
+// reset the contents of the container to remove previous result.
+container.innerHTML = '';
+    
+// create a template for character details.
+// popuate it with data 
+let characterDetails = document.createElement('div');
+characterDetails.classList.add('characterDetails');
+
+// Used toUpperCase() from https://www.w3schools.com/jsref/jsref_touppercase.asp to try and change the display name of the Pokemon.
+// let str = pokemon.name;
+// let test = str.toUpperCase();
+
+let imageblock = 
+  `<p class="name">${pokemon.name}</p>
+  <img class="pokepic" src="${pokemon.sprites.other.home.front_shiny}">
+  `;
+  for (type of pokemon.types) {
+    console.log(type)
+    imageblock +=  `
+    <div class="widget types">
+    <label>Type:</label>
+    <p class="type">${type.type.name}
+    </p>
+    </div>`;
+  }
+  // for (ability of pokemon.abilities) {
+  //   console.log(ability)
+  //   imageblock += `<p class="ability">${ability.ability.name}</p>`;
+  // }
+  for (encounter of encounters) {
+    console.log(encounter)
+    imageblock += `
+    <div class="widget types">
+    <label>Location:</label>
+    <p class="location">${encounter.location_area.name} <br> Encounter Chance: ${encounter.version_details[0].encounter_details[0].chance}%</p>
+    </div>
+    `;
+  }
+  characterDetails.innerHTML = imageblock
+
+// add everything to the page.
+container.appendChild(characterDetails);
     })
 
-    // reset the contents of the container to remove previous result.
-    container.innerHTML = '';
-    
-    // create a template for character details.
-    // popuate it with data 
-    let characterDetails = document.createElement('div');
-    characterDetails.classList.add('characterDetails'); 
-    characterDetails.innerHTML = 
-      `<img src="${pokemon.sprites.front_shiny}">
-      <p>${pokemon.name}</p>`;
+  })
 
-    // add everything to the page.
-    container.appendChild(characterDetails); 
-
+  
 
   }
